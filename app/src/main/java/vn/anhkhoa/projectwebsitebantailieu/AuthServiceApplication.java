691b7223de532cbbfd12b9cbafd28281021f0d50
anhkhoa2109.it@gmail.com <anhkhoa2109.it@gmail.com>
2025-01-27 13:35:02 +0700

    Cập nhật giao diện màn hình đăng nhập, đăng ký, quên mật khẩu và xác nhận OTP

---
+++ app/src/main/java/vn/anhkhoa/projectwebsitebantailieu/AuthServiceApplication.java
@@ -0,0 +1,5 @@
+package vn.anhkhoa.projectwebsitebantailieu;
+
+public class AuthServiceApplication {
+
+}
---
+++ app/src/main/java/vn/anhkhoa/projectwebsitebantailieu/MainActivity.java
@@ -0,0 +1,52 @@
+package vn.anhkhoa.projectwebsitebantailieu;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSignIn;
+    TextView txtForgetPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        //anh xa
+        btnSignIn = (Button) findViewById(R.id.btnSignin);
+        txtForgetPass = (TextView) findViewById(R.id.txtForgotPassword);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        txtForgetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ForgetPassword.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
